f1b39abf5e7860c6be004528301573f4126676db
eozturk@microstrategy.com <haktan_emir@yahoo.com>
2023-05-13 22:12:48 +0200

    chore: update form

--- src/components/gpt-prompt/gpt-form.tsx
+++ src/components/gpt-prompt/gpt-form.tsx
@@ -1,42 +1,27 @@
 /* eslint-disable @typescript-eslint/no-misused-promises */
-
-import React, {
-  type ChangeEvent,
-  useCallback,
-  type Dispatch,
-  type SetStateAction,
-} from "react";
+import React, { type ChangeEvent } from "react";
 
 interface GptFormProps {
   onSubmit: (e: React.FormEvent<HTMLFormElement>) => Promise<void>;
-  setQuery: Dispatch<SetStateAction<string>>;
-  query: string;
+  userInput: string;
+  onFormInputChange: ({ target }: ChangeEvent<HTMLInputElement>) => void;
 }
 
-const GptForm = ({ onSubmit, setQuery, query }: GptFormProps) => {
-  const onInputChange = useCallback(
-    ({ target }: ChangeEvent<HTMLInputElement>) => {
-      setQuery(target.value);
-    },
-    [setQuery]
-  );
+const GptForm = ({ onSubmit, userInput, onFormInputChange }: GptFormProps) => (
+  <div className="flex h-1/5 w-full">
+    <form onSubmit={(e) => onSubmit(e)}>
+      <input
+        type="text"
+        name="gpt-query"
+        placeholder="Please enter your query here."
+        title="gpt-query-input"
+        onChange={onFormInputChange}
+        value={userInput}
+      />
 
-  return (
-    <div className="flex h-1/5 w-full">
-      <form onSubmit={(e) => onSubmit(e)}>
-        <input
-          type="text"
-          name="gpt-query"
-          placeholder="Please enter your query here."
-          title="gpt-query-input"
-          onChange={onInputChange}
-          value={query}
-        />
-
-        <button type="submit">Submit</button>
-      </form>
-    </div>
-  );
-};
+      <button type="submit">Submit</button>
+    </form>
+  </div>
+);
 
 export default GptForm;
