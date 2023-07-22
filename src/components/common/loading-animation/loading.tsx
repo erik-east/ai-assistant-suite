0506658109794d3517ef35c85188af09634768ed
eozturk@microstrategy.com <haktan_emir@yahoo.com>
2023-07-22 20:38:53 +0200

    chore: Add loading animation for cover letter, storytime and greetify

--- src/components/common/loading-animation/loading.tsx
+++ src/components/common/loading-animation/loading.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import Lottie from "react-lottie";
 import { defaultAnimationProps } from "@/utils/lottie-service";
+
 import * as wanderlustLoadingAnimation from "../../../../public/assets/journey-planner-loading-animation.json";
 import * as essayWriterLoadingAnimation from "../../../../public/assets/essay-writer-loading-animation.json";
+import * as resumeLoadingAnimation from "../../../../public/assets/resume-loading.json";
+import * as greetingLoadingAnimation from "../../../../public/assets/greeting-loading-animation.json";
+import * as storyLoadingAnimation from "../../../../public/assets/story-loading-animation.json";
+
 import { ProjectTypeEnums } from "@/utils/types";
 
 interface LoadingProps {
@@ -11,16 +16,16 @@ interface LoadingProps {
 
 export const Loading: React.FC<LoadingProps> = ({ projectType }) => {
   const loadingAnimation = {
-    [ProjectTypeEnums.GREETIFY_COMPANION]: wanderlustLoadingAnimation, // TODO: Update loading animation for greetify companion loader
-    [ProjectTypeEnums.STORYTIME_COMPANION]: wanderlustLoadingAnimation, // TODO: Update loading animation for storytime companion loader
+    [ProjectTypeEnums.GREETIFY_COMPANION]: greetingLoadingAnimation,
+    [ProjectTypeEnums.STORYTIME_COMPANION]: storyLoadingAnimation,
     [ProjectTypeEnums.WANDERLUST_COMPANION]: wanderlustLoadingAnimation,
     [ProjectTypeEnums.WORDSMITH_COMPANION]: essayWriterLoadingAnimation,
+    [ProjectTypeEnums.COVER_LETTER_COMPANION]: resumeLoadingAnimation,
   };
 
   return (
     <>
       <div className="fixed bottom-0 left-0 right-0 top-0 z-10 flex h-screen w-full flex-col items-center justify-center overflow-hidden bg-gray-700 opacity-75"></div>
-      {/* Separated divs to fix opacity issue in the animation */}
       <div className="fixed bottom-0 left-0 right-0 top-0 z-20 flex h-screen w-full flex-col items-center justify-center overflow-hidden">
         <Lottie
           options={defaultAnimationProps(loadingAnimation[projectType])}
